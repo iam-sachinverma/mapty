1f2e3e2ec812fb84653f9e338e8ca834b60d33ef
iam-sachinverma <sachinvermab@gmail.com>
2022-07-24 22:37:08 +0530

    Managing Workout Data_ Create Workout , Running , Cycling Classes

--- script.js
+++ script.js
@@ -11,9 +11,44 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-// Global Var
-let map, mapEvent;
+class Workout {
+  date = new Date();
+  id = (new Date() + '').slice(-10);
 
+  constructor(coords, distance, duration) {
+    this.coords = coords;
+    this.distance = distance; //km
+    this.duration = duration; //min
+  }
+}
+
+class Running extends Workout {
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcPace();
+  }
+
+  calcPace() {
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
+}
+
+class Cycling extends Workout {
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain;
+    this.calcSpeed();
+  }
+
+  calcSpeed() {
+    this.speed = this.distance / (this.duration / 60);
+    return this.speed;
+  }
+}
+
+// APPLICATION ARCHITECTURE
 class App {
   // private data member
   #map;
